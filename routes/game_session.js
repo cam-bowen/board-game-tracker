0708e3f60207e61b5fd38abb804d3af13ba204a1
cbowen04 <modernwhitekid@gmail.com>
2023-12-17 15:20:53 -0500

    clean up

--- routes/game_session.js
+++ routes/game_session.js
@@ -6,18 +6,8 @@ module.exports = {
 	},
 	postAdd: (req, res) => {
 		console.log(req.body);
-
-		//req.connect(function(err) {
-			//if (err) throw err;
-			//req.query("SELECT * FROM customers", function (err, result, fields) {
-			 //if (err) throw err;
-			 // console.log(result);
-			//})
-		//)};
 			
 		// TODO db.query to insert game-playing session
-
-		// should I do a render?
 
 		res.redirect('/');
 	}
