07d46e2ceaabe8071be7f0b8760027225f14c4ae
cbowen04 <modernwhitekid@gmail.com>
2023-12-27 20:47:12 -0500

    schema.sql changes

--- schema.sql
+++ schema.sql
@@ -7,11 +7,11 @@
         Recent_Date_Played NVARCHAR(50) NOT NULL
     );
 
-# TODO: add sample data
-INSERT INTO game_info(ID, Game, Days_Played, Recent_Date_Played)
+# TODO: add sample data, I entered these seperate, you do not really need to this, just use the app
+INSERT INTO game_info(Game, Days_Played, Recent_Date_Played)
 VALUES 
-    (1, "Monopoly", 3, "12/23/22");
-    (2, "Phase Ten", 2, "12.15.22");
-    (3, "Yahtzee", 4, "12.29.22");
-    (4, "Life", 10, "1.13.23");
-    (5, "Poker", 2, "1.22.23");
+    ("Monopoly", 3, "12/23/22");
+    ("Phase Ten", 2, "12.15.22");
+    ("Yahtzee", 4, "12.29.22");
+    ("Life", 10, "1.13.23");
+    ("Poker", 2, "1.22.23");
